c7bf463b0ad494c6110049883bd8ab9046d7dd38
Luis Cerritos <luis.cerritos2011@gmail.com>
2018-07-27 10:12:19 -0600

    .

--- src/JPanel/jpReminder.java
+++ src/JPanel/jpReminder.java
@@ -28,17 +28,6 @@ public class jpReminder extends javax.swing.JPanel {
         jPanel2 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         tbReminder = new javax.swing.JTable();
-        jCalendar1 = new com.toedter.calendar.JCalendar();
-        jLabel2 = new javax.swing.JLabel();
-        txtReminder = new javax.swing.JTextField();
-        jLabel3 = new javax.swing.JLabel();
-        txtFecha = new javax.swing.JTextField();
-        jLabel4 = new javax.swing.JLabel();
-        txtHora = new javax.swing.JTextField();
-        jLabel5 = new javax.swing.JLabel();
-        jBInsertar = new javax.swing.JButton();
-        txtId = new javax.swing.JTextField();
-        jLabel6 = new javax.swing.JLabel();
         btnEliminar = new javax.swing.JButton();
         btnActualizar = new javax.swing.JButton();
 
@@ -89,63 +78,13 @@ public class jpReminder extends javax.swing.JPanel {
         ));
         jScrollPane1.setViewportView(tbReminder);
 
-        jLabel2.setText("Recordatorio:");
-
-        jLabel3.setText("Fecha de Recordatorio: ");
-
-        jLabel4.setText("Repetir dentro de ");
-
-        txtHora.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyPressed(java.awt.event.KeyEvent evt) {
-                txtHoraKeyPressed(evt);
-            }
-            public void keyReleased(java.awt.event.KeyEvent evt) {
-                txtHoraKeyReleased(evt);
-            }
-            public void keyTyped(java.awt.event.KeyEvent evt) {
-                txtHoraKeyTyped(evt);
-            }
-        });
-
-        jLabel5.setText("horas");
-
-        jBInsertar.setText("Modificar");
-        jBInsertar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jBInsertarActionPerformed(evt);
-            }
-        });
-
-        jLabel6.setText("Id:");
-
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 763, Short.MAX_VALUE)
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.TRAILING)
-                                .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING))
-                            .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jLabel6, javax.swing.GroupLayout.Alignment.TRAILING))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                .addComponent(jBInsertar)
-                                .addComponent(txtReminder)
-                                .addComponent(txtFecha, javax.swing.GroupLayout.PREFERRED_SIZE, 204, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(jPanel2Layout.createSequentialGroup()
-                                .addComponent(txtHora, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jLabel5))
-                            .addComponent(txtId, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jCalendar1, javax.swing.GroupLayout.PREFERRED_SIZE, 320, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 763, Short.MAX_VALUE)
                 .addContainerGap())
         );
         jPanel2Layout.setVerticalGroup(
@@ -153,31 +92,7 @@ public class jpReminder extends javax.swing.JPanel {
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 206, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addComponent(jCalendar1, javax.swing.GroupLayout.DEFAULT_SIZE, 272, Short.MAX_VALUE)
-                        .addContainerGap())
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(txtId, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel6))
-                        .addGap(27, 27, 27)
-                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(txtReminder, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel2))
-                        .addGap(18, 18, 18)
-                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel3)
-                            .addComponent(txtFecha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(25, 25, 25)
-                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel4)
-                            .addComponent(txtHora, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel5))
-                        .addGap(38, 38, 38)
-                        .addComponent(jBInsertar))))
+                .addContainerGap(289, Short.MAX_VALUE))
         );
 
         btnEliminar.setBackground(new java.awt.Color(0, 102, 255));
@@ -245,61 +160,15 @@ public class jpReminder extends javax.swing.JPanel {
        
     }//GEN-LAST:event_btnActualizarActionPerformed
 
-    private void txtHoraKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtHoraKeyPressed
-        // TODO add your handling code here:zzz
-    }//GEN-LAST:event_txtHoraKeyPressed
-
-    private void txtHoraKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtHoraKeyReleased
-
-    }//GEN-LAST:event_txtHoraKeyReleased
-
-    private void txtHoraKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtHoraKeyTyped
-        char c=evt.getKeyChar();
-        if(c<'0' || c>'9') evt.consume();
-    }//GEN-LAST:event_txtHoraKeyTyped
-
-    private void jBInsertarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBInsertarActionPerformed
-
-        if(txtFecha.getText().isEmpty() || txtReminder.getText().isEmpty()|| txtHora.getText().isEmpty() || txtId.getText().isEmpty()){
-            JOptionPane.showMessageDialog(this,"Campos vacios");
-        } else{
-            classReminder.setId((Integer.parseInt(txtId.getText())));
-            classReminder.setReminder((txtReminder.getText()));
-            classReminder.setDatetime((txtFecha.getText()));
-            classReminder.setRepeat((Integer.parseInt(txtHora.getText())));
-
-            if(classReminder.updatereminder()){
-                JOptionPane.showMessageDialog(this, "Recordatorio actualizado");
-            }else{
-                JOptionPane.showMessageDialog(this, "Error al actualizar recordatorio");
-            }
-
-        }
-
-        standardization.hideaddReminder(TechnoNizer.home);
-        tbReminder.setModel(classReminder.cargarReminder());
-    }//GEN-LAST:event_jBInsertarActionPerformed
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnActualizar;
     private javax.swing.JButton btnAgregar;
     private javax.swing.JButton btnEliminar;
-    private javax.swing.JButton jBInsertar;
-    private com.toedter.calendar.JCalendar jCalendar1;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tbReminder;
-    private javax.swing.JTextField txtFecha;
-    private javax.swing.JTextField txtHora;
-    private javax.swing.JTextField txtId;
-    private javax.swing.JTextField txtReminder;
     // End of variables declaration//GEN-END:variables
 }
