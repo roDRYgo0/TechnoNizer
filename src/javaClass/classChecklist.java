fa46c26de28a3f841ca7225965b69e217c08ce51
Elian González <elian8@outlook.es>
2018-07-27 10:06:34 -0600

    Cruds

---
+++ src/javaClass/classChecklist.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaClass;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class classChecklist {
+
+    
+    
+    private static Integer id;
+    private static String title;
+    private static Integer idBelongs;
+    private static Integer condition;
+    
+    
+    //<editor-fold defaultstate="collapsed" desc="Getter y Setter">
+    
+
+    public static Integer getId() {
+        return id;
+    }
+
+    public static void setId(Integer id) {
+        classChecklist.id = id;
+    }
+
+    public static String getTitle() {
+        return title;
+    }
+
+    public static void setTitle(String title) {
+        classChecklist.title = title;
+    }
+
+    public static Integer getIdBelongs() {
+        return idBelongs;
+    }
+
+    public static void setIdBelongs(Integer idBelongs) {
+        classChecklist.idBelongs = idBelongs;
+    }
+
+    public static Integer getCondition() {
+        return condition;
+    }
+
+    public static void setCondition(Integer condition) {
+        classChecklist.condition = condition;
+    }
+    //</editor-fold>
+    
+    
+  
+}
---
+++ src/javaClass/classChecks.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaClass;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class classChecks {
+    
+    private static Integer id;
+    private static String contents;
+    private static Integer condition;
+    private static Integer visible;
+    private static Integer idChecklist;
+
+    //<editor-fold defaultstate="collapsed" desc="Getter y Setter">
+    
+
+    
+    public static Integer getId() {
+        return id;
+    }
+
+    public static void setId(Integer id) {
+        classChecks.id = id;
+    }
+
+    public static String getContents() {
+        return contents;
+    }
+
+    public static void setContents(String contents) {
+        classChecks.contents = contents;
+    }
+
+    public static Integer getCondition() {
+        return condition;
+    }
+
+    public static void setCondition(Integer condition) {
+        classChecks.condition = condition;
+    }
+
+    public static Integer getVisible() {
+        return visible;
+    }
+
+    public static void setVisible(Integer visible) {
+        classChecks.visible = visible;
+    }
+
+    public static Integer getIdChecklist() {
+        return idChecklist;
+    }
+
+    public static void setIdChecklist(Integer idChecklist) {
+        classChecks.idChecklist = idChecklist;
+    }
+  
+    //</editor-fold>
+    
+}
