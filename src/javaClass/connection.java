1e05b9dafc9661a455980d9241f97f321977750a
Elian González <elian8@outlook.es>
2018-09-27 20:15:00 -0600

    Saves

--- src/javaClass/connection.java
+++ src/javaClass/connection.java
@@ -16,7 +16,7 @@ public class connection {
 
             String url = "jdbc:sqlserver://localhost;databaseName=BD_TechnoNizer";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            on = DriverManager.getConnection(url,"technonizer","Masupial.48");
+            on = DriverManager.getConnection(url,"technonizerdb","Masupial.48");
         }catch(ClassNotFoundException | SQLException e){
             System.out.println(e.getMessage());
         }
